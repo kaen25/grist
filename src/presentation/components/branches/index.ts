aa1365f66f5e633f11aff76b225f56eeea74980a
kaen <kaen25@gmail.com>
2026-01-02 09:46:18 +0100

    feat: add RebaseDialog component

--- src/presentation/components/branches/index.ts
+++ src/presentation/components/branches/index.ts
@@ -3,3 +3,4 @@ export { BranchList } from './BranchList';
 export { BranchItem } from './BranchItem';
 export { CreateBranchDialog } from './CreateBranchDialog';
 export { MergeDialog } from './MergeDialog';
+export { RebaseDialog } from './RebaseDialog';
